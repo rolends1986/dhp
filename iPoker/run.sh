d844e63577bab584d0838b2484a2db73cf3d0989
rolends1986 <rolends1986@gmail.com>
2020-04-02 23:47:46 +0800

    Update run.sh

--- iPoker/run.sh
+++ iPoker/run.sh
@@ -3,7 +3,7 @@ export HOST_IP=118.107.46.16
 export PROJECT_NAME=iPoker
 export COMPOSE_PROJECT_NAME=$PROJECT_NAME  
 export ETCD_HOST=http://$HOST_IP:2379
-
+rm -rf $PROJECT_NAME
 mkdir $PROJECT_NAME
 cd $PROJECT_NAME
 wget  https://raw.githubusercontent.com/rolends1986/dhp/master/iPoker/docker-compose.yml
