b212e263387331b88104d9a14d5d073fa116935c
rolends1986 <rolends1986@gmail.com>
2020-04-02 23:43:21 +0800

    Create run.sh

---
+++ iPoker/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export PROJECT_NAME=iPoker
+export COMPOSE_PROJECT_NAME=$PROJECT_NAME 
+export HOST_IP=118.107.46.16
+export ETCD_HOST=http://118.107.46.16:2379
+
+mkdir $PROJECT_NAME
+cd $PROJECT_NAME
+wget  
+
+
+cd ..
+ 
+ 
